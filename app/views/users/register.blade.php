fb366635a5d43ef997ba3fba759b35cd4429167e
Diana Gamez <diana.gamez.fon@gmail.com>
2014-02-10 11:10:05 -0600

    nuevogit add .git add .

--- app/views/users/register.blade.php
+++ app/views/users/register.blade.php
@@ -1,20 +1,61 @@
-{{ Form::open(array('url'=>'users/create', 'class'=>'form-signup')) }}
-   <h2 class="form-signup-heading">Registro</h2>
+@extends('layouts.masterview')
+
+   @section('title')
+      @parent
+         :: Register
+   @stop
+   
+   @section('content')
+
+      {{ Form::open(array('url'=>'users/create', 'class'=>'form-signup')) }}
+         <h2 class="form-signup-heading">Register</h2>
+         <ul>
+            @foreach($errors->all() as $error)
+               <li>{{ $error }}</li>
+            @endforeach
+         </ul>
+
+         <div class="form-group">
+            {{ Form::label('firstname', 'First Name') }}
+            {{ Form::text('firstname', null, array('class' => 'form-control')) }}
+          </div>
  
-   <ul>
-      @foreach($errors->all() as $error)
-         <li>{{ $error }}</li>
-      @endforeach
-   </ul>
+         <div class="form-group">
+            {{ Form::label('lastname', 'Last Name') }}
+            {{ Form::text('lastname', null, array('class' => 'form-control')) }}
+         </div>
  
-   {{ Form::text('firstname', null, array('class'=>'input-block-level', 'placeholder'=>'First Name')) }}
-   {{ Form::text('lastname', null, array('class'=>'input-block-level', 'placeholder'=>'Last Name')) }}
-   {{ Form::text('email', null, array('class'=>'input-block-level', 'placeholder'=>'Email Address')) }}
-   {{ Form::text('username', null, array('class'=>'input-block-level', 'placeholder'=>'User Name')) }}
-   {{ Form::password('password', array('class'=>'input-block-level', 'placeholder'=>'Password')) }}
-   {{ Form::password('password_confirmation', array('class'=>'input-block-level', 'placeholder'=>'Confirm Password')) }}
-   {{ Form::text('rol', null, array('class'=>'input-block-level', 'placeholder'=>'Rol de usuario')) }}
-   {{ Form::text('status', null, array('class'=>'input-block-level', 'placeholder'=>'Status')) }}
+         <div class="form-group">
+            {{ Form::label('email', 'Email') }}
+            {{ Form::email('email', null, array('class' => 'form-control')) }}
+         </div>
  
-   {{ Form::submit('Register', array('class'=>'btn btn-large btn-primary btn-block'))}}
-{{ Form::close() }}
+         <div class="form-group">
+            {{ Form::label('username', 'Username') }}
+            {{ Form::text('username', null, array('class' => 'form-control')) }}
+         </div>
+ 
+         <div class="form-group">
+            {{ Form::label('password', 'Password') }}
+            {{ Form::password('password', null, array('class' => 'form-control')) }}
+         </div>
+ 
+         <div class="form-group">
+            {{ Form::label('password_confirmation', 'Confirm password') }}
+            {{ Form::password('password_confirmation', null, array('class' => 'form-control')) }}
+         </div>
+ 
+         <div class="form-group">
+            {{ Form::label('rol', 'Type') }}
+            {{ Form::select('rol', array('0' => 'Select a type', 'admin' => 'Administrador', 'hotel' => 'Hotel', 'restaurant' => 'Restaurant', 'nightlife' => 'Nightlife', 'shopping' => 'Shopping', 'tour' => 'Tour', 'beach' => 'Beach', 'event' => 'Event', 'transportation' => 'Transportation', 'doctor' => 'Doctor'), null, array('class' => 'form-control')) }}
+         </div>
+ 
+         <div class="form-group">
+            {{ Form::label('status', 'Status') }}
+            {{ Form::select('status', array('0' => 'Select a status', 'active' => 'Active', 'inactive' => 'Inactive'), null, array('class' => 'form-control')) }}
+         </div>
+
+         {{ Form::submit('Register', array('class'=>'btn btn-large btn-primary btn-block'))}}
+      {{ Form::close() }}
+
+   @stop
