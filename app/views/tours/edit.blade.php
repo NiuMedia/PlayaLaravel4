abdcc8e91a3522efb72885c5ea371306139d9d39
Diana Gamez <diana.gamez.fon@gmail.com>
2014-02-14 13:02:56 -0600

    vistas completas

---
+++ app/views/tours/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.masterdashuser')
+
+@section('title')
+  @parent
+     :: Edit
+@stop
+
+@section('content')
+	
+	<h1>Edit</h1>
+	{{ HTML::ul($errors->all()) }}
+	{{ Form::model($tour, array('route' => array('app.tours.update', $tour->id), 'method' => 'PUT')) }}
+		<div class="form-group">
+	        {{ Form::label('name', 'Beach Name') }}
+	        {{ Form::text('name', null, array('class' => 'form-control')) }}
+	    </div>
+
+     	<div class="form-group">
+        	{{ Form::label('address', 'Address') }}
+        	{{ Form::text('address', null, array('class' => 'form-control')) }}
+     	</div>
+
+     	<div class="form-group">
+        	{{ Form::label('overview', 'Overview') }}
+        	{{ Form::text('overview', null, array('class' => 'form-control', 'rows' => '3')) }}
+     	</div>
+
+	{{ Form::submit('Edit', array('class'=>'btn btn-large btn-primary btn-block'))}}
+  {{ Form::close() }}
+@stop
---
+++ app/views/transports/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.masterdashuser')
+
+@section('title')
+  @parent
+     :: Dashboard
+@stop
+
+@section('content')
+
+	<h1>Transportation Dashboard</h1>
+
+	<p>Welcome {{Auth::user()->firstname}} {{Auth::user()->lastname}}</p>
+
+	<?php
+		$id = Transport::where('id_user', '=', Auth::user()->id)->pluck('id');
+		$name = Transport::where('id_user', '=', Auth::user()->id)->pluck('name'); 
+		$address = Transport::where('id_user', '=', Auth::user()->id)->pluck('address');
+		$overview = Transport::where('id_user', '=', Auth::user()->id)->pluck('overview');
+	?>
+
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>Attribute</th>
+				<th>Content</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Contact Person</td>
+				<td>{{Auth::user()->firstname}} {{Auth::user()->lastname}}</td>
+			</tr>
+			<tr>
+				<td>Email Contact Person</td>
+				<td>{{Auth::user()->email}}</td>
+			</tr>
+			<tr>
+				<td>Beach name</td>
+				<td>{{$name}}</td>
+			</tr>
+			<tr>
+				<td>Address</td>
+				<td>{{$address}}</td>
+			</tr>
+			<tr>
+				<td>Overview</td>
+				<td>{{$overview}}</td>
+			</tr>
+		</tbody>
+	</table>
+
+	<a class="btn btn-small btn-success" style="width:100%;" href="{{ URL::to('app/beaches/' . $id . '/edit' ) }}">Edit my info</a>	
+
+
+@stop
